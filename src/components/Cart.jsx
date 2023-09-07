94ea6e06c13eb32893b100a3002ed484650896cc
Cathy Rolfs <crolfs47@gmail.com>
2023-09-07 11:10:43 -0500

    Add styling to cart items

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,8 +2,8 @@ import "../styles/Cart.css";
 
 const Cart = ({ cart }) => {
   const totalCost = cart.reduce((total, item) => {
-    return total + (item.product.price * item.quantity)
-  }, 0)
+    return total + item.product.price * item.quantity;
+  }, 0);
 
   return (
     <>
@@ -15,15 +15,25 @@ const Cart = ({ cart }) => {
           return (
             <div className="cart-item-card" key={item.product.id}>
               <img src={item.product.image} alt="" />
-              <div>
-                <h5>{item.product.name}</h5>
-                <span>${cost}</span>
+              <div className="product-details">
+                <div className="product-name-price">
+                  <h5>{item.product.name}</h5>
+                  <h5>${cost}</h5>
+                </div>
+                <div className="product-quantity">
+                  <div className="qty-div">
+                    <button className="qty-button">-</button>
+                    <span className="qty-input">{item.quantity}</span>
+                    <button className="qty-button">+</button>
+                  </div>
+                  <button className="remove-button">REMOVE</button>
+                </div>
               </div>
             </div>
           );
         })}
+        <div className="subtotal">Subtotal: ${totalCost}</div>
       </div>
-      <div>${totalCost}</div>
     </>
   );
 };
